4b2a7d2c1b3816d44fe01f271073cb5b06c5d81b
eric wang <eric@future-grid.com.au>
2017-12-01 16:50:50 +1100

    fix bug: endless loop.

--- dist/fgp.kit.bundle.js
+++ dist/fgp.kit.bundle.js
@@ -4567,7 +4567,8 @@ fgpWidgetGraph.prototype.controller = function controller ($scope, $element, $wi
                                         if ($scope.rangeConfig) {
                                             $scope.rangeConfig.dateWindow = [new Date(range[1] - currentInterval.interval), range[1]];
                                         }
-                                        $scope.chartDateTime = {begin:new Date(range[1] - currentInterval.interval),end: range[1]};
+
+                                        $scope.currentChart.updateOptions({dateWindow: [new Date(range[1] - currentInterval.interval), range[1]]});
                                         $scope.currentIntervalChoosed = currentInterval;
                                     } else {
                                         // send the date window back to outside.
@@ -4595,7 +4596,8 @@ fgpWidgetGraph.prototype.controller = function controller ($scope, $element, $wi
                                     if ($scope.rangeConfig) {
                                         $scope.rangeConfig.dateWindow = [new Date(newValue.start), new Date(newValue.end)];
                                     }
-                                    $scope.chartDateTime = {begin:new Date(range[1] - currentInterval.interval),end: range[1]};
+
+                                    $scope.currentChart.updateOptions({dateWindow:[new Date(newValue.start), new Date(newValue.end)]});
                                     $scope.currentIntervalChoosed = currentInterval;
                                     // }
                                 } else {
--- src/com/futuregrid/platform/kit/show/directives/fgp.graph.js
+++ src/com/futuregrid/platform/kit/show/directives/fgp.graph.js
@@ -3823,7 +3823,8 @@ class fgpWidgetGraph {
                                             if ($scope.rangeConfig) {
                                                 $scope.rangeConfig.dateWindow = [new Date(range[1] - currentInterval.interval), range[1]];
                                             }
-                                            $scope.chartDateTime = {begin:new Date(range[1] - currentInterval.interval),end: range[1]};
+
+                                            $scope.currentChart.updateOptions({dateWindow: [new Date(range[1] - currentInterval.interval), range[1]]});
                                             $scope.currentIntervalChoosed = currentInterval;
                                         } else {
                                             // send the date window back to outside.
@@ -3851,7 +3852,8 @@ class fgpWidgetGraph {
                                         if ($scope.rangeConfig) {
                                             $scope.rangeConfig.dateWindow = [new Date(newValue.start), new Date(newValue.end)];
                                         }
-                                        $scope.chartDateTime = {begin:new Date(range[1] - currentInterval.interval),end: range[1]};
+
+                                        $scope.currentChart.updateOptions({dateWindow:[new Date(newValue.start), new Date(newValue.end)]});
                                         $scope.currentIntervalChoosed = currentInterval;
                                         // }
                                     } else {
